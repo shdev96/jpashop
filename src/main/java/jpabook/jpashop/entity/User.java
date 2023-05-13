28cdbae16388ccd815dd87b73a0518d4a2db1522
이승희 <dev@iseunghuiui-noteubug.local>
2023-05-13 18:49:09 +0900

    init

---
+++ src/main/java/jpabook/jpashop/entity/User.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
